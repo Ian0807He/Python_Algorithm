618fc58618ee87101645451e18d5759129bae35b
IanPeverell <517382432@qq.com>
2016-11-21 16:11:42 +0800

    QuickSort

--- QuickSort.py
+++ QuickSort.py
@@ -1,4 +1,4 @@
-def QuickSort(L):
-    if len(L)<=1 :
-        return L
-    return QuickSort([lt for lt in L[1:] if lt < L[0]]) + [L[0]] + QuickSort([ge for ge in L[1:] if ge >= L[0]])
+def QuickSort(List):
+    if len(List)<=1 :
+        return List
+    return QuickSort([lt for lt in List[1:] if lt < List[0]]) + [List[0]] + QuickSort([ge for ge in List[1:] if ge >= List[0]])
