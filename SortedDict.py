6d3ccc41315b82f4de6f826505f58122b2ee42f6
IanPeverell <517382432@qq.com>
2016-11-21 16:33:25 +0800

    SortedDict

--- SortedDict.py
+++ SortedDict.py
@@ -1,5 +1,9 @@
+def SortedDict(dict, wrap="key"):
+    if wrap is "key":
+        return sorted(d.items(), key=lambda t: t[0])
+    if wrap is "value":
+        return sorted(d.items(), key=lambda t: t[1])
+
 d = {'banana': 3, 'apple':4,'pear': 1, 'orange': 2}
-a = sorted(d.items(), key=lambda t: t[0])
-print a
-b = sorted(d.items(), key=lambda t: t[1])
-print b
+print(SortedDict(d))
+print(SortedDict(d,"value"))
