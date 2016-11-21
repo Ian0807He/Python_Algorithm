b63dd1cac1c7abd16cee1fca1b1a7441791bcf8c
IanPeverell <517382432@qq.com>
2016-11-21 22:52:52 +0800

    SingleLinkedList

--- LinkedList.py
+++ LinkedList.py
@@ -1,28 +1,70 @@
 class Node(object):
-  def __init__(self,item,next):
-    self.item=item
-    self.next=next
-  def getItem(self):
-    return self.item
-  def getNext(self):
-    return self.next
-  def setItem(self,newitem):
-    self.item=newitem
+    def __init__(self,item,next):
+        self.item=item
+        self.next=next
+
+    def getItem(self):
+        return self.item
+
+    def getNext(self):
+        return self.next
+
+    def setNext(self, newitem):
+        self.next = newitem
+
+    def setItem(self,newitem):
+        self.item=newitem
 
 class LinkedList(object):
     def __init__(self):
         self.head = None
+        self.curr = None
+        self.last = None
 
-    def add(self, item):
-        self.head = Node(item, self.head)
-
-    def remove(self):
-        if self.is_empty():
-            return None
+    def addEnd(self, item):
+        self.curr = Node(item, None)
+        if self.head is None:
+            self.head = self.curr
         else:
-            item = self.head.item
-            self.head = self.head.next
-            return item
+            self.last.setNext(self.curr)
+        self.last = self.curr
+
+    def addHead(self, item):
+        if self.head:
+            self.head = Node(item, self.head)
+        else:
+            self.head = Node(item, None)
 
     def isEmpty(self):
         return self.head == None
+
+    def printList(self):
+        data = []
+        if self.isEmpty():
+            return False
+        curr = self.head
+        data.append(curr.getItem())
+        while curr.getNext():
+            curr = curr.getNext()
+            data.append(curr.getItem())
+        return data
+
+    def search(self, item):
+        if self.isEmpty():
+            yield False
+        else:
+            curr = self.head
+            find = False
+            num = 1
+            if curr.getItem() is item:
+                yield num
+                num += 1
+                find = True
+            while curr.getNext():
+                curr = curr.getNext()
+                num += 1
+                if curr.getItem() is item:
+                    yield num
+                    find = True
+            if not find:
+                yield False
---
+++ test2.py
@@ -0,0 +1,16 @@
+from LinkedList import *
+
+list = LinkedList()
+list.addEnd(1)
+list.addEnd(2)
+list.addEnd(3)
+list.addEnd('a')
+list.addEnd(3)
+list.addEnd('b')
+print(list.printList())
+
+list.addHead(5)
+print(list.printList())
+
+for i in list.search(3):
+    print(i)
